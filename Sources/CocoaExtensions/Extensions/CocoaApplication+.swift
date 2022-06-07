de2743d1b2803f27e3515a6acee43bf27924e8a6
maximkrouk <id.maximkrouk@gmail.com>
2022-06-08 01:10:55 +0200

    feat: App key window search improvements

--- Sources/CocoaExtensions/Extensions/CocoaApplication+.swift
+++ Sources/CocoaExtensions/Extensions/CocoaApplication+.swift
@@ -1,9 +1,22 @@
-#if !os(watchOS)
 import CocoaAliases
+import FoundationExtensions
 
+#if os(macOS)
 extension CocoaApplication {
   public var firstKeyWindow: CocoaWindow? {
-      windows.first(where: { $0.isKeyWindow })
+    windows.first(where: { $0.isKeyWindow })
+  }
+}
+#elseif !os(watchOS)
+extension UIApplication {
+  var keyWindow: UIWindow? {
+    return connectedScenes
+      .filter { $0.activationState == .foregroundActive }
+      .first { $0 is UIWindowScene }
+      .flatMap { $0 as? UIWindowScene }
+      .map(\.windows)
+      .flatMap { $0.first(where: \.isKeyWindow) }
+      .or(windows.first(where: \.isKeyWindow))
   }
 }
 #endif
