1d790872b108dfd2dcc82cd5a4711b212e119d7a
maximkrouk <id.maximkrouk@gmail.com>
2026-03-04 16:20:35 +0100

    feat: LayerView.onLayout

--- Sources/CocoaExtensions/Components/LayerView/LayerView.swift
+++ Sources/CocoaExtensions/Components/LayerView/LayerView.swift
@@ -4,5 +4,13 @@ import CocoaAliases
 open class LayerView<Layer: CALayer>: CustomCocoaView {
 	open override class var layerClass: AnyClass { Layer.self }
 	public var actualLayer: Layer! { layer as? Layer }
+
+	public var onLayout: ((Layer) -> Void)?
+
+	public override func layoutSublayers(of layer: CALayer) {
+		super.layoutSublayers(of: layer)
+		guard actualLayer === layer else { return }
+		onLayout?(actualLayer)
+	}
 }
 #endif
