3ae3501b822f23d708a9daec7a69c01076e2c5c3
maximkrouk <id.maximkrouk@gmail.com>
2026-05-28 23:20:01 +0200

    feat(Custom): Add CustomCollectionViewFlowLayout

---
+++ Sources/CocoaExtensions/Custom/Other/CustomCocoaCollectionFlowLayout.swift
@@ -0,0 +1,15 @@
+import CocoaAliases
+
+open class CustomCocoaCollectionViewFlowLayout: CocoaCollectionViewFlowLayout {
+	public override init() {
+		super.init()
+		self._init()
+	}
+
+	public required init?(coder: NSCoder) {
+		super.init(coder: coder)
+		self._init()
+	}
+
+	open func _init() {}
+}
