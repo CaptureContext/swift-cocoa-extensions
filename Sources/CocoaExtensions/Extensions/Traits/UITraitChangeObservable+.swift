5e6991d6d664c20a476c5762918d4f87622ffe7b
maximkrouk <id.maximkrouk@gmail.com>
2026-07-13 17:07:02 +0200

    Improve trait change observation

--- Sources/CocoaExtensions/Extensions/Traits/UITraitChangeObservable+.swift
+++ Sources/CocoaExtensions/Extensions/Traits/UITraitChangeObservable+.swift
@@ -22,9 +22,10 @@ where Self: NSObject & UITraitEnvironment {
 	}
 
 	@discardableResult
-	public func observeTrait<Trait: UITraitDefinition>(
+	private func _observeTrait<Trait: UITraitDefinition>(
 		_ trait: Trait.Type,
-		handler: ((Self, Trait.Value) -> Void)?
+		handler: ((Self, Trait.Value) -> Void)?,
+		value: @escaping (UITraitCollection, Trait.Type) -> Trait.Value
 	) -> (any UITraitChangeRegistration)? {
 		guard let handler else {
 			activeTraitRegistrations[ObjectIdentifier(Trait.self)] = nil
@@ -33,12 +34,136 @@ where Self: NSObject & UITraitEnvironment {
 
 		let registration = self.registerForTraitChanges(
 			[Trait.self]
-		) { (_self: Self, collection: UITraitCollection) in
-			handler(_self, self.traitCollection[Trait.self])
+		) { (_self: Self, _: UITraitCollection) in
+			handler(_self, value(_self.traitCollection, Trait.self))
 		}
 
 		activeTraitRegistrations[ObjectIdentifier(Trait.self)] = registration
 		return registration
+	}
+
+	@discardableResult
+	public func observeTrait<Trait: UITraitDefinition>(
+		_ trait: Trait.Type,
+		handler: ((Self, Trait.Value) -> Void)?
+	) -> (any UITraitChangeRegistration)? where Trait.Value == CGFloat {
+		self._observeTrait(trait, handler: handler) { collection, trait in
+			collection[trait]
+		}
+	}
+
+	@discardableResult
+	public func observeTrait<Trait: UITraitDefinition>(
+		_ trait: Trait.Type,
+		handler: ((Self, Trait.Value) -> Void)?
+	) -> (any UITraitChangeRegistration)? where Trait.Value == CGFloat? {
+		self._observeTrait(trait, handler: handler) { collection, trait in
+			collection[trait]
+		}
+	}
+
+	@discardableResult
+	public func observeTrait<Trait: UITraitDefinition>(
+		_ trait: Trait.Type,
+		handler: ((Self, Trait.Value) -> Void)?
+	) -> (any UITraitChangeRegistration)? where Trait.Value == Double {
+		self._observeTrait(trait, handler: handler) { collection, trait in
+			collection[trait]
+		}
+	}
+
+	@discardableResult
+	public func observeTrait<Trait: UITraitDefinition>(
+		_ trait: Trait.Type,
+		handler: ((Self, Trait.Value) -> Void)?
+	) -> (any UITraitChangeRegistration)? where Trait.Value == Double? {
+		self._observeTrait(trait, handler: handler) { collection, trait in
+			collection[trait]
+		}
+	}
+
+	@discardableResult
+	public func observeTrait<Trait: UITraitDefinition>(
+		_ trait: Trait.Type,
+		handler: ((Self, Trait.Value) -> Void)?
+	) -> (any UITraitChangeRegistration)? where Trait.Value == Int {
+		self._observeTrait(trait, handler: handler) { collection, trait in
+			collection[trait]
+		}
+	}
+
+	@discardableResult
+	public func observeTrait<Trait: UITraitDefinition>(
+		_ trait: Trait.Type,
+		handler: ((Self, Trait.Value) -> Void)?
+	) -> (any UITraitChangeRegistration)? where Trait.Value == Int? {
+		self._observeTrait(trait, handler: handler) { collection, trait in
+			collection[trait]
+		}
+	}
+
+	@discardableResult
+	public func observeTrait<Trait: UITraitDefinition>(
+		_ trait: Trait.Type,
+		handler: ((Self, Trait.Value) -> Void)?
+	) -> (any UITraitChangeRegistration)? where Trait.Value == Bool {
+		self._observeTrait(trait, handler: handler) { collection, trait in
+			collection[trait]
+		}
+	}
+
+	@discardableResult
+	public func observeTrait<Trait: UITraitDefinition>(
+		_ trait: Trait.Type,
+		handler: ((Self, Trait.Value) -> Void)?
+	) -> (any UITraitChangeRegistration)?
+	where Trait.Value: RawRepresentable {
+		self._observeTrait(trait, handler: handler) { collection, trait in
+			collection[trait]
+		}
+	}
+
+	@discardableResult
+	public func observeTrait<Trait: UITraitDefinition>(
+		_ trait: Trait.Type,
+		handler: ((Self, Trait.Value) -> Void)?
+	) -> (any UITraitChangeRegistration)?
+	where Trait.Value: RawRepresentable, Trait.Value.RawValue == CGFloat {
+		self._observeTrait(trait, handler: handler) { collection, trait in
+			collection[trait]
+		}
+	}
+
+	@discardableResult
+	public func observeTrait<Trait: UITraitDefinition>(
+		_ trait: Trait.Type,
+		handler: ((Self, Trait.Value) -> Void)?
+	) -> (any UITraitChangeRegistration)?
+	where Trait.Value: RawRepresentable, Trait.Value.RawValue == Double {
+		self._observeTrait(trait, handler: handler) { collection, trait in
+			collection[trait]
+		}
+	}
+
+	@discardableResult
+	public func observeTrait<Trait: UITraitDefinition>(
+		_ trait: Trait.Type,
+		handler: ((Self, Trait.Value) -> Void)?
+	) -> (any UITraitChangeRegistration)?
+	where Trait.Value: RawRepresentable, Trait.Value.RawValue == Int {
+		self._observeTrait(trait, handler: handler) { collection, trait in
+			collection[trait]
+		}
+	}
+
+	@discardableResult
+	public func observeTrait<Trait: UITraitDefinition>(
+		_ trait: Trait.Type,
+		handler: ((Self, Trait.Value) -> Void)?
+	) -> (any UITraitChangeRegistration)? {
+		self._observeTrait(trait, handler: handler) { collection, trait in
+			collection[trait]
+		}
 	}
 }
 #endif
